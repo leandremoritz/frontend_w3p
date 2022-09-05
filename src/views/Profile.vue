43a28f2193bdd519c6cac4d957122f77923ece0f
leandremoritz <leandremoritz19@gmail.com>
2022-09-05 16:26:39 +0200

    updateUser

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -87,8 +87,7 @@ li {
   background-color: brown;
   border-radius: 24px;
   border-style: none;
-  box-shadow: rgba(0, 0, 0, 0.2) 0 3px 5px -1px,
-    rgba(0, 0, 0, 0.14) 0 6px 10px 0, rgba(0, 0, 0, 0.12) 0 1px 18px 0;
+  filter: drop-shadow(0px 0px 5px pink);
   box-sizing: border-box;
   color: #3c4043;
   cursor: pointer;
